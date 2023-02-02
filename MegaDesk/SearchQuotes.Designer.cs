f2e7a9daafea2226be3bb4711cfbccf379aa4b8b
Jose Alarcon <alarconjj@byui.edu>
2023-02-01 22:59:26 -0700

    The conditional is ready

--- MegaDesk/SearchQuotes.Designer.cs
+++ MegaDesk/SearchQuotes.Designer.cs
@@ -28,33 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchQuotes));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.newQuoteLabelTitle = new System.Windows.Forms.Label();
+            this.labelDrawers = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBoxMaterialSearch = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // pictureBox1
             // 
-            this.button1.Location = new System.Drawing.Point(356, 148);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 59);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // newQuoteLabelTitle
+            // 
+            this.newQuoteLabelTitle.AutoSize = true;
+            this.newQuoteLabelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newQuoteLabelTitle.Location = new System.Drawing.Point(71, 12);
+            this.newQuoteLabelTitle.Name = "newQuoteLabelTitle";
+            this.newQuoteLabelTitle.Size = new System.Drawing.Size(132, 24);
+            this.newQuoteLabelTitle.TabIndex = 3;
+            this.newQuoteLabelTitle.Text = "Search quotes";
+            // 
+            // labelDrawers
+            // 
+            this.labelDrawers.AutoSize = true;
+            this.labelDrawers.Location = new System.Drawing.Point(72, 36);
+            this.labelDrawers.Name = "labelDrawers";
+            this.labelDrawers.Size = new System.Drawing.Size(328, 13);
+            this.labelDrawers.TabIndex = 9;
+            this.labelDrawers.Text = "You have the ability to search a quote based on the surface material";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 361);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // comboBoxMaterialSearch
+            // 
+            this.comboBoxMaterialSearch.FormattingEnabled = true;
+            this.comboBoxMaterialSearch.Location = new System.Drawing.Point(571, 36);
+            this.comboBoxMaterialSearch.Name = "comboBoxMaterialSearch";
+            this.comboBoxMaterialSearch.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMaterialSearch.TabIndex = 11;
+            this.comboBoxMaterialSearch.SelectedIndexChanged += new System.EventHandler(this.comboBoxMaterialSearch_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(568, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Surface Material";
             // 
             // SearchQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxMaterialSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelDrawers);
+            this.Controls.Add(this.newQuoteLabelTitle);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "SearchQuotes";
             this.Text = "SearchQuotes";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SearchQuotes_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label newQuoteLabelTitle;
+        private System.Windows.Forms.Label labelDrawers;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBoxMaterialSearch;
+        private System.Windows.Forms.Label label1;
     }
 }
--- MegaDesk/SearchQuotes.cs
+++ MegaDesk/SearchQuotes.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static MegaDesk.DeskQuote;
 
 namespace MegaDesk
 {
@@ -15,12 +19,59 @@ namespace MegaDesk
         public SearchQuotes()
         {
             InitializeComponent();
-        }
 
+            List<Desk.DesktopMaterial> surface_materials = Enum.GetValues(typeof(Desk.DesktopMaterial)).Cast<Desk.DesktopMaterial>().ToList();
+            // Populate combo box with Surface list
+            comboBoxMaterialSearch.DataSource = surface_materials;
+            comboBoxMaterialSearch.SelectedIndex = -1;
+
+            // List<Desk.DesktopMaterial> surface_materials2 = Enum.GetValues(typeof(Desk.DesktopMaterial)).Cast<Desk.DesktopMaterial>().ToList();
+            //List<Desk.DesktopMaterial> x = Enum.GetValues(typeof(Desk.DesktopMaterial)).Cast<Desk.DesktopMaterial>().ToList();
+            //label1.Text = ;
+        }
         private void SearchQuotes_FormClosed(object sender, FormClosedEventArgs e)
         {
             MainMenu formMainMenu = (MainMenu)Tag;
             formMainMenu.Show();
+        }
+
+        private void comboBoxMaterialSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string materials = comboBoxMaterialSearch.Text; /// NECESITO CONSEGUIR EL VALOR DE LA VARIABLE MATERIALS QUE ES EL NOMBRE DEL MATERIAL
+            try
+            {
+                string fileJson = File.ReadAllText(@"files\quotes.json");
+                List<DeskQuote> lstQuote = JsonConvert.DeserializeObject<List<DeskQuote>>(fileJson);
+               
+               DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[7] 
+                    {   new DataColumn("Date and Time", typeof(DateTime)), 
+                        new DataColumn("Customer Name", typeof(string)), 
+                        new DataColumn("Rush Date", typeof(DeskQuote.RUSH_DAYS)),
+                        new DataColumn("Width", typeof(int)),
+                        new DataColumn("Depth", typeof(int)),
+                        new DataColumn("Number of Drawers", typeof(int)),
+                        new DataColumn("Surface Material", typeof(int)),
+                    });
+
+                for (int i = 1; i < lstQuote.Count; i++)
+                {
+                    if (lstQuote[i].SurfaceMaterial == Convert.ToInt32(materials)) // AQUI VA A IR EL STRING materials PARA COMPARAR LA SUPERFICIE DEL MATERIAL
+                    {
+                       dt.Rows.Add(lstQuote[i].dateTime, lstQuote[i].CustomerName, lstQuote[i].RushDays, lstQuote[i].Width, lstQuote[i].Depth, lstQuote[i].NumberOfDrawers, lstQuote[i].SurfaceMaterial);
+                    
+                    }
+                   
+                }
+
+                dataGridView1.DataSource = dt;
+                
+            }
+            catch (Exception ex)
+            {
+                //Ignored
+            }
         }
     }
 }
