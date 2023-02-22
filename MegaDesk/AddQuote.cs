2b5e6ce5823bfd6e013c1317b20e0c47fef7e954
wmezadev <williameduardomeza@gmail.com>
2023-02-21 23:08:56 -0500

    week8: fix revert changes for AddQuote.cs in Desktop App

--- MegaDesk/AddQuote.cs
+++ MegaDesk/AddQuote.cs
@@ -1,175 +1,181 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
+using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static MegaDesk.Desk;
+using System.Data;
 
 namespace MegaDesk
 {
-    public class DeskQuote
+    public partial class AddQuote : Form
     {
-        public const double BASE_PRICE = 200;
-        public const double PRICE_PER_IN2 = 1;
-        public const double PRICE_PER_DRAWER = 50;
-        public const double SURFACE_MINIMUM = 1000;
-        public enum RUSH_DAYS
+        public AddQuote()
         {
-            [Description("3 days")]
-            Rush3,
-            [Description("5 days")]
-            Rush5,
-            [Description("7 days")]
-            Rush7,
-            [Description("14 days, no Rush")]
-            Rush14
-        }
+            InitializeComponent();
+            // Set current Date in label
+            DateTime dateTime = DateTime.Now;
+            labelCurrentDate.Text = dateTime.ToString("dd/MM/yyyy");
+            comboBoxDrawers.SelectedIndex = 0;
 
-        public Desk desk;
+            List<Desk.DesktopMaterial> surface_materials = Enum.GetValues(typeof(Desk.DesktopMaterial)).Cast<Desk.DesktopMaterial>().ToList();
+            // Populate combo box with Surface list
+            comboBoxMaterial.DataSource = surface_materials;
+            comboBoxMaterial.SelectedIndex = -1;
 
-        public DateTime dateTime { get; set; }
-        public string CustomerName { get; set; }
-        public RUSH_DAYS RushDays { get; set; }
-
-        public int Width { get; set; }
-        public int Depth { get; set; }
-        public int NumberOfDrawers { get; set; }
-        public int SurfaceMaterial { get; set; }
-
-
-
-        //public DateTime dateTime = DateTime.Now;
-        public DeskQuote(Desk desk, string customerName, RUSH_DAYS rushDays)
-        {
-            this.desk = desk;
-            Width = desk.Width;
-            Depth = desk.Depth;
-            NumberOfDrawers = desk.NumberOfDrawers;
-            SurfaceMaterial = (int)desk.SurfaceMaterial;
-            dateTime = DateTime.Now;
-            CustomerName = customerName;
-            RushDays = rushDays;
-        }
-
-        // Code from https://stackoverflow.com/questions/2650080/how-to-get-c-sharp-enum-description-from-value
-        public static string GetEnumDescription(Enum value)
-        {
-            FieldInfo fi = value.GetType().GetField(value.ToString());
-            DescriptionAttribute[] attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
-            if (attributes != null && attributes.Any())
+            List<DeskQuote.RUSH_DAYS> rush_days_options_list = Enum.GetValues(typeof(DeskQuote.RUSH_DAYS)).Cast<DeskQuote.RUSH_DAYS>().ToList();
+            List<string> rushOptions = new List<string>();
+            for (int i = 0; i < rush_days_options_list.Count; i++)
             {
-                return attributes.First().Description;
+                string rushOption = DeskQuote.GetEnumDescription((DeskQuote.RUSH_DAYS)i);
+                rushOptions.Add(rushOption);
             }
-            return value.ToString();
+            // Populate combo box with Rush days list
+            comboBoxRush.DataSource = rushOptions;
+            comboBoxRush.SelectedIndex = -1;
+        }
+        private void buttonCancelQuote_Click(object sender, System.EventArgs e)
+        {
+            Close();
         }
 
-        public double GetBasePrice()
+        private void AddQuote_FormClosed(object sender, FormClosedEventArgs e)
         {
-            return BASE_PRICE;
+            MainMenu formMainMenu = (MainMenu)Tag;
+            formMainMenu.Show();
         }
-
-        public double GetAreaPrice()
+        public bool IsValidAddQuoteForm()
         {
-            double price = 0;
-            if (desk.GetArea() > SURFACE_MINIMUM)
-            {
-                price = desk.GetArea() - SURFACE_MINIMUM;
-            }
-            return price;
-        }
-
-        public double GetMaterialPrice()
-        {
-            return (double)desk.SurfaceMaterial;
-        }
-
-        public double GetDrawerPrice()
-        {
-            return PRICE_PER_DRAWER * desk.NumberOfDrawers;
-        }
-
-        public double GetRushPrice()
-        {
-            // If there is no rush (14 day) skip calc and return 0
-            if (RushDays.Equals(RUSH_DAYS.Rush14))
-            {
-                return 0;
-            }
-            // price file
-            string[] pricesFromFile = GetRushOrder();
-            // convert file into two dimensions arrays
-            int[,] rushOrderPrices = PreparePricesFromFile(pricesFromFile);
-            // default area position (area less than 1000)
-            int areaColumn = 0;
-            if (desk.GetArea() >= 1000 && desk.GetArea() <= 2000)
-            {
-                areaColumn = 1;
-            }
-            else if (desk.GetArea() > 2000)
-            {
-                areaColumn = 2;
-            }
-            // return price using rush days and area position
-            return rushOrderPrices[(int)RushDays, areaColumn];
-        }
-
-
-        private string[] GetRushOrder()
-        {
-            string path = @"files\rushOrderPrices.txt";
-            List<string> prices = new List<string>();
             try
             {
-                if (File.Exists(path))
+                if (string.IsNullOrEmpty(textBoxCustomer.Text))
                 {
-                    string[] readText = File.ReadAllLines(path);
-                    prices = new List<string>(readText.Length);
-                    foreach (string price in readText)
-                    {
-                        prices.Add(price);
-                    }
-                    return prices.ToArray();
+                    throw new Exception("Customer name should not be empty");
+                }
+                else if (string.IsNullOrEmpty(textBoxWidth.Text) || !(Convert.ToInt32(textBoxWidth.Text) >= Desk.MIN_WIDTH && Convert.ToInt32(textBoxWidth.Text) <= Desk.MAX_WIDTH))
+                {
+                    throw new Exception("You must give a width between " + Desk.MIN_WIDTH + " and " + Desk.MAX_WIDTH);
+                }
+                else if (string.IsNullOrEmpty(textBoxDepth.Text) || !(Convert.ToInt32(textBoxDepth.Text) >= Desk.MIN_DEPTH && Convert.ToInt32(textBoxDepth.Text) <= Desk.MAX_DEPTH))
+                {
+                    throw new Exception("You must give a depth between " + Desk.MIN_DEPTH + " and " + Desk.MAX_DEPTH);
+                }
+                else if (comboBoxMaterial.SelectedIndex == -1)
+                {
+                    throw new Exception("You must select a Surface Material");
+                }
+                else if (comboBoxRush.SelectedIndex == -1)
+                {
+                    throw new Exception("You must select a Rush Day");
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+                return false;
+            }
+        }
+
+        private void buttonSaveQuote_Click(object sender, EventArgs e)
+        {
+            if (!IsValidAddQuoteForm())
+            {
+                return;
+            }
+            Desk desk = new Desk(Convert.ToInt32(textBoxWidth.Text), Convert.ToInt32(textBoxDepth.Text), Convert.ToInt32(comboBoxDrawers.SelectedItem.ToString()), (Desk.DesktopMaterial)comboBoxMaterial.SelectedItem);
+            DeskQuote deskQuote = new DeskQuote(desk, textBoxCustomer.Text, (DeskQuote.RUSH_DAYS)comboBoxRush.SelectedIndex);
+
+            DisplayQuote formDisplayQuote = new DisplayQuote(deskQuote);
+            try
+            {
+                if (File.Exists("files/quotes.json"))
+                {
+                    string oldJson = File.ReadAllText("files/quotes.json");
+                    List<DeskQuote> displayQuotes = JsonConvert.DeserializeObject<List<DeskQuote>>(oldJson);
+                    displayQuotes.Add(deskQuote);
+
+                    string json = JsonConvert.SerializeObject(displayQuotes, Formatting.Indented);
+
+                    File.WriteAllText("files/quotes.json", json);
 
                 }
                 else
                 {
-                    throw new Exception("File doesnt exist");
+                    List<DeskQuote> deskQuotes = new List<DeskQuote> { deskQuote };
+                    string json = JsonConvert.SerializeObject(deskQuotes, Formatting.Indented);
+
+                    File.WriteAllText("files/quotes.json", json);
                 }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
 
             }
-            catch (FileNotFoundException e)
-            {
-                MessageBox.Show(e.Message);
-            }
-            return prices.ToArray(); //Default value is empy
+
+            // Pass This Tag
+            formDisplayQuote.Tag = this.Tag;
+            formDisplayQuote.Show(this);
+            Hide();
         }
 
-        private int[,] PreparePricesFromFile(string[] pricesFromFile)
+        private void textBoxWidth_Validating(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            int[,] prices = new int[3, 3];
-            double row = 0;
-            int counter = 0;
-            for (int i = 0; i < pricesFromFile.Length; i++)
+            bool isNumber = int.TryParse(textBoxWidth.Text, out int width);
+            if (isNumber)
             {
-                int price = int.Parse(pricesFromFile[i]);
-                prices[(int)row, counter] = price;
-
-                counter++;
-                if ((i + 1) % 3 == 0)
+                if (width.ToString().Length > 0)
                 {
-                    row++;
-                    counter = 0;  // every 3 items I want to restart this counter in order to fill the array
+                    if (width < 24)
+                    {
+                        e.Cancel = true;
+                        setErrorToField(textBoxWidth, Color.Red, "Minimum value is " + Desk.MIN_WIDTH + " inches");
+
+                        return;
+                    }
+                    else if (width > 94)
+                    {
+                        e.Cancel = true;
+                        setErrorToField(textBoxWidth, Color.Red, "Maximum value is " + Desk.MAX_WIDTH + " inches");
+
+                        return;
+                    }
+                    removeErrorField(textBoxWidth);
                 }
             }
-            return prices;
+            else
+            {
+                e.Cancel = true;
+                setErrorToField(textBoxWidth, Color.Red, "Please enter a number minor than " + Desk.MAX_WIDTH + " and major than " + Desk.MIN_WIDTH);
+            }
         }
-        public double GetTotalPrice()
+
+        private void setErrorToField(TextBox inputField, Color errorColor, string errorMessge)
         {
-            return GetBasePrice() + GetAreaPrice() + GetMaterialPrice() + GetDrawerPrice() + GetRushPrice();
+            inputField.BackColor = errorColor;
+            //Set focus to recived input
+            inputField.Select(0, inputField.Text.Length);
+            // Set the ErrorProvider error with the text to display. 
+            this.errorProvider1.SetError(inputField, errorMessge);
+        }
+
+        private void removeErrorField(TextBox inputField)
+        {
+            this.errorProvider1.SetError(inputField, "");
+            inputField.BackColor = Color.White;
+        }
+
+        private void textBoxDepth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
