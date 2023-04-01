eb34cc1677ca5da592cfab1669f9b670c7a44677
Jose Alarcon <alarconjj@byui.edu>
2023-04-01 11:30:22 -0600

    Controller and view added

---
+++ SacramentMeetingPlanner/SacramentMeetingPlanner/Controllers/SacramentMeetingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SacramentMeetingPlanner.Controllers
+{
+    public class SacramentMeetingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
