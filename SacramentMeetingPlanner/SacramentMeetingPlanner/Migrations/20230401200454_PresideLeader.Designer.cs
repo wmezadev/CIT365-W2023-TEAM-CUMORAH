23c96539c8d0b68b26a2fadede34e91067fa5ff1
Jose Alarcon <alarconjj@byui.edu>
2023-04-01 14:42:13 -0600

    SeedData ready

--- SacramentMeetingPlanner/SacramentMeetingPlanner/Migrations/20230401200454_PresideLeader.Designer.cs
+++ SacramentMeetingPlanner/SacramentMeetingPlanner/Migrations/20230401200454_PresideLeader.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SacramentMeetingPlanner.Data;
 
@@ -11,9 +12,11 @@ using SacramentMeetingPlanner.Data;
 namespace SacramentMeetingPlanner.Migrations
 {
     [DbContext(typeof(SacramentMeetingPlannerContext))]
-    partial class SacramentMeetingPlannerContextModelSnapshot : ModelSnapshot
+    [Migration("20230401200454_PresideLeader")]
+    partial class PresideLeader
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
